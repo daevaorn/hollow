3f79b650402529aaa07d24fb30fee70c370f690b
Leonid Shvechikov <shvechikov@yandex-team.ru>
2011-06-29 18:24:01 +0400

    added a couple of fake transaction method

--- hollow/backend/base.py
+++ hollow/backend/base.py
@@ -2,6 +2,12 @@ from django.db.backends.dummy.base import *
 from django.db.backends.dummy.base import DatabaseWrapper as DummyDatabaseWrapper
 
 class DatabaseWrapper(DummyDatabaseWrapper):
+    def managed(self, *args, **kwargs):
+        pass
+
+    def enter_transaction_management(self, *args, **kwargs):
+        pass
+
     def _enter_transaction_management(self, *args, **kwargs):
         pass
 
